3ed3f77f01ba455bc1c87ff2c5190494c0eec0e9
JoseBernal06 <jose.bernal@epn.edu.ec>
2024-10-08 15:20:59 -0500

    cuarta version

--- fundamentos.js
+++ fundamentos.js
@@ -31,11 +31,9 @@ let datos={
 
 //console.log(datos.age);
 
-
-
 //------------------------------------------------------------------------------
-// strings
-// propiedades y metodos
+
+// ?strings (propiedades y metodos)
 
 let password="password";
 
@@ -47,9 +45,137 @@ console.log(password.toLocaleUpperCase()); //convierte todo en mayusculas
 console.log(password.toLocaleLowerCase()); //convierte todo en minusculas
 
 
-
 //ES6 - nuevas caracteristicas
 console.log(`El password es: ${password}`);
+
+//-----------------------------------------------------------------------------
+
+// ?Casting - ES6
+let number_uno="100.15"
+let number_dos=100
+
+console.log(+number_uno+number_dos);
+
+//jerarquia de operadores (* / + - ** ())
+
+//-----------------------------------------------------------------------------
+
+// ?Condiciones
+let moneda_uno=12;
+let moneda_dos=15;
+
+if (moneda_uno<=moneda_dos) {
+    console.log("uno")
+
+}else{
+    console.log("dos");
+}
+
+// ES6 - operador ternario
+moneda_uno<=moneda_dos ? console.log("uno") : console.log("dos");
+
+let nombre;
+!nombre ? console.log("Sin nombre") : console.log("Hola");
+
+//----------------------------------------------------------------------------
+
+// ?Valores falso y verdaderos
+
+//comparacion estricta
+let vidas='6';
+vidas === 6 ? console.log("Pasa nivel") : console.log("Sigue");
+vidas == 6 ? console.log("Pasa nivel") : console.log("Sigue");
+
+//---------------------------------------------------------------------------
+
+// ?Loops
+
+const goals=["Mateo", "Juan", "Marcelo", "Maria"]
+
+goals.forEach(x => {
+    console.log(x);
+});
+
+
+
+for (let i = 0; i < goals.length; i++) {
+    const element = goals[i];
+    console.log(element);
+}
+
+newGoals = goals.map((x)=>x.toLocaleUpperCase());
+console.log(newGoals);
+
+//-----------------------------------------------------------------------------
+
+// ?Funciones
+// funciones autoejecutadas y anonimas
+(function() {
+    console.log("Hola mundo")
+})
+()
+
+//Funciones declaradas
+function getGokuAvatar() {
+    console.log("photo/avatar/goku.png")
+}
+getGokuAvatar();
+
+
+//Funciones expresadas
+const gokuBatalla = function() {
+    console.log("Listo para pelear")
+}
+gokuBatalla()
+
+
+//Funcion flecha
+const batalla=()=>{
+    console.log("..........Adios")
+    //nfn
+}
+batalla()
+
+const poder = (x) => { console.log("Golllllll") }
+poder()
+
+//------------------------------------------------------------------------------
+
+// ?Argumentos
+
+const email=function(mail, token) {
+    console.log(`El token ${token} del email`)
+}
+email("mateo@gmail.com", 1546)
+
+
+
+// Argumetos opcionales
+const email_dos=function(token, mail="Mateo@gmail.com"){
+    console.log(`El token ${token} del email ${mail}`)
+}
+email_dos(1546)
+
+//Retorno
+function retornoCapitulo() {
+    //return "Nuevo Capitulo";
+    //return 5;
+    //return {nombre: "Hola", episodio: 74}
+    return function() {
+        return 5
+    }
+}
+console.log(retornoCapitulo()())
+
+
+//----------------------------------------------------------------------------
+
+//Objetos
+
+let usuario = {
+    nombre:"Jose",
+    edad:20
+}
 
 
 
