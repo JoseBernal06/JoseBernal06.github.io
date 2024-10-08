aa99b8dceb22e2945af276103d197d55acf943af
JoseBernal06 <jose.bernal@epn.edu.ec>
2024-10-08 13:46:29 -0500

    tercera version

--- fundamentos.js
+++ fundamentos.js
@@ -8,26 +8,48 @@
 // tipos de datos
 let data1=42; 
 let data2= 2.5;
-let data3="Mateo" 
-let data4=false
+let data3="Mateo" ;
+let data4=false;
 let data5; 
-let data6 ='@' //char
-let data7 = null //null
+let data6 ='@' ;//char
+let data7 = null; //null
 
-console.log(data2);
+//console.log(data2);
 
 
 //tipos de datos
-let data8=["Mateo", "Juan", "Marcelo"]; //Array
+
+//Array
+let data8=["Mateo", "Juan", "Marcelo", 4]; 
 
 // objeto
-let data9= datos={
+let datos={
     name:"Mateo",
-    city:"Quito"
+    city:"Quito",
+    age:20
 }
 
+//console.log(datos.age);
 
 
+
+//------------------------------------------------------------------------------
+// strings
+// propiedades y metodos
+
+let password="password";
+
+//Metodos
+console.log(password.length);
+console.log(password.trim()); //remueve los espacios en blancos de las cadenas de texto
+console.log(password.includes('6')); //compara si hay un elemento en la cadena de texto (valor booleano)
+console.log(password.toLocaleUpperCase()); //convierte todo en mayusculas
+console.log(password.toLocaleLowerCase()); //convierte todo en minusculas
+
+
+
+//ES6 - nuevas caracteristicas
+console.log(`El password es: ${password}`);
 
 
 
